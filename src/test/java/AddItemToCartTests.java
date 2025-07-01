dfa6b1d368591a872ebb4badd5b2b5f98bf300ef
T.Tatsenko <tatsenko.tetiana@gmail.com>
2025-07-01 18:26:30 +0200

    addItemToCartPositiveTest is reviewed

--- src/test/java/AddItemToCartTests.java
+++ src/test/java/AddItemToCartTests.java
@@ -5,10 +5,10 @@ public class AddItemToCartTests extends TestBase{
 
     @Test
     public void addItemToCartPositiveTest(){
-        click(By.xpath("//a[normalize-space()='Books']"));
-        click(By.xpath("//input[@value='Add to cart']"));
+        click(By.linkText("14.1-inch Laptop"));
+        click(By.id("add-to-cart-button-31"));
         click(By.xpath("//span[normalize-space()='Shopping cart']"));
-        assertElementPresent(By.xpath("//a[@class='product-name' and normalize-space()='Computing and Internet']"),
-                "The product link 'Computing and Internet' is not displayed");
+        assertElementPresent(By.xpath("//a[@class='product-name' and normalize-space()='14.1-inch Laptop']"),
+                "14.1-inch Laptop' is not displayed");
     }
 }
