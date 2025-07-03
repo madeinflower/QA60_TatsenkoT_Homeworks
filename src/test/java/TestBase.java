e525762d83b085dd79814213befde7970dbc3124
T.Tatsenko <tatsenko.tetiana@gmail.com>
2025-07-04 00:03:03 +0200

    logback-classic added

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,20 +1,21 @@
 import com.demowebshop.fw.ApplicationManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.Browser;
-import org.testng.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-import java.time.Duration;
+import java.lang.reflect.Method;
+
 
 public class TestBase {
 
-    WebDriver driver;
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
     protected static ApplicationManager app =
             new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
@@ -28,6 +29,22 @@ public class TestBase {
     public void tearDown(){
         app.stop();
     }
+    @BeforeMethod
+    public void startTest(Method method){
+        logger.info("Start test" + method.getName() );
+    }
 
+    @AfterMethod
+    public void stopTest(ITestResult result)
+    {
+        if(result.isSuccess()){
+            logger.info("PASSED: "+result.getMethod().getMethodName());
+        }
+        else {
+            logger.error("FAILED: "+result.getMethod().getMethodName());
+        }
+        logger.info("Stop test");
+        logger.info("====================================================");
+    }
 
 }
